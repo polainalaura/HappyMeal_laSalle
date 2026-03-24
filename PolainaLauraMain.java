2b15f6df17cbe193bab7e86920000256c83a5012
polainalaura <92717040+polainalaura@users.noreply.github.com>
2026-03-24 21:50:48 +0100

    Refactor meal selection and update prices

--- PolainaLauraMain.java
+++ PolainaLauraMain.java
@@ -6,33 +6,38 @@ public class PolainaLauraMain {
         Scanner sc = new Scanner(System.in);
         Menu menu = new Menu();
 
-
+        System.out.println("Bienvenido a LaSalleKidsHappyMeal");
 
         String[] platos = {"Hamburguesa", "Nuggets", "Chicken Burger"};
-        double[] preciosPlatos = {2.50, 1.00, 99.99}; 
-
+        double[] preciosPlatos = {2.50, 2.00, 3.10};
 
         System.out.println("Elige plato:");
-        int opcionPlato = sc.nextInt();
+        int opcionPlato = sc.nextInt(); 
 
+        menu.setPlatoPrincipal(platos[opcionPlato], preciosPlatos[opcionPlato]);
 
-        menu.setPlatoPrincipal(platos[opcionPlato + 1], preciosPlatos[opcionPlato + 1]);
-
-
-        String[] complementos = {"Patatas fritas", "Ensalda", "Tomates"};
-        double[] preciosComplementos = {0, 0, 0};
-        int opcionComple = 5; 
+        String[] complementos = {"Patatas fritas", "Ensalda", "Tomates cherry"}; 
+        double[] preciosComplementos = {1.50, 1.80, 1.40};
+        int opcionComple = 1;
 
         menu.setComplemento(complementos[opcionComple], preciosComplementos[opcionComple]);
 
+        String[] postres = {"Manzana", "Piña", "Brocheta de fruta"};
+        double[] preciosPostres = {1.00, 1.20, 1.50};
 
-        String[] postres = {};
-        double[] preciosPostres = {};
+        int opcionPostre = 2;
+        menu.setPostre(postres[opcionPostre], preciosPostres[opcionPostre]);
 
-        menu.setPostre(postres[0], preciosPostres[0]);
+        String[] bebidas = {"Agua", "Zumo naranja", "Zumo piña"};
+        double[] preciosBebidas = {1.00, 1.20, 1.30}; 
 
+        int opcionBebida = 0;
+        menu.setBebida(bebidas[opcionBebida], preciosBebidas[opcionBebida]);
 
-        menu.setBebida(null, -10); 
+        menu.seleccionarSorpresa();
+        System.out.println("\n=== RESUMEN DEL PEDIDO ===");
         menu.mostrarResumen();
+
+        System.out.println("\n¡Gracias por usar LaSalleKidsHappyMeal!");
     }
 }
